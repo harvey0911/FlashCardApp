104e5b2f0e65149a9ce5e8e9ce7fe3040b51bea4
derkaoui-larbi <larbi.derkaoui01@gmail.com>
2023-12-09 20:34:01 +0100

    Login and register Pages, Needs modifs to be the homepage and then after the auth we get redirected to the mainpage of our app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart' show BuildContext, Key, MaterialApp, StatelessWidget, Widget, runApp;
+import 'package:flutter_flashcards/Authentication/main_screens/LoginPage.dart';
 import 'package:flutter_flashcards/notifiers/flashcards_notifier.dart';
 import 'package:flutter_flashcards/notifiers/review_notifier.dart';
 import 'package:flutter_flashcards/notifiers/settings_notifier.dart';
@@ -30,3 +31,21 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+//class Home extends StatelessWidget {
+  //@override
+  //Widget build(BuildContext context) {
+    //return MaterialApp(
+      //debugShowCheckedModeBanner: false,
+      //theme: ThemeData(fontFamily: 'Abel'),
+      //initialRoute: RegisterPage.id,
+      //routes: {
+        //RegisterPage.id: (context) => RegisterPage(),
+        //LoginPage.id: (context) => LoginPage(),
+        //ForgotPassword.id: (context) => ForgotPassword(),
+        //Done.id: (context) => Done(),
+      //},
+    //);
+  //}
+
+
